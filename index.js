e7193d9f3a084afb2a56694d36bf8e74d81b042b
Caleb <calebcowen@gmail.com>
2018-10-09 22:05:21 -0500

    add module

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
-const quickStream = () => {
-  if (navigator.mediaDevices === undefined) {
-    navigator.mediaDevices = {};
-  }
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
 
-  if (navigator.mediaDevices.getUserMedia === undefined) {
-    navigator.mediaDevices.getUserMedia = (constraints) => {
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = (constraints) => {
 
-      let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+    let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia
 
-      if (!getUserMedia) {
-        return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
-      }
-
-      return new Promise((resolve, reject) => {
-        getUserMedia.call(navigator, constraints, resolve, reject)
-      })
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
     }
-  }
 
-  const getWebRTCVideoElement = async (height, width, video) => {
-    let isIOS = navigator.userAgent.includes('iPhone') || navigator.userAgent.includes('iPad')
-    let promise = isIOS ? getWebRTCStream() : getWebRTCStream({video: {height, width}})
-    promise.then((stream) => {
-      video.srcObject = stream
-      if (isIOS) {
-        video.height = height
-        video.width = width
-      }
+    return new Promise((resolve, reject) => {
+      getUserMedia.call(navigator, constraints, resolve, reject)
     })
-  }
-
-  const getWebRTCStream = async (e, constraints={video: true, audio: true}) => {
-    let stream = await navigator.mediaDevices.getUserMedia(constraints)
-    .then((stream) => stream)
-    .catch((err) => err)
-    return stream
   }
 }
 
-module.exports = quickStream
+const getWebRTCVideoElement = async (height, width, video) => {
+  let isIOS = navigator.userAgent.includes('iPhone') || navigator.userAgent.includes('iPad')
+  let promise = isIOS ? getWebRTCStream() : getWebRTCStream({video: {height, width}})
+  promise.then((stream) => {
+    video.srcObject = stream
+    if (isIOS) {
+      video.height = height
+      video.width = width
+    }
+  })
+}
+
+const getWebRTCStream = async (e, constraints={video: true, audio: true}) => {
+  let stream = await navigator.mediaDevices.getUserMedia(constraints)
+  .then((stream) => stream)
+  .catch((err) => err)
+  return stream
+}
+
+module.exports = {
+  getWebRTCStream,
+  getWebRTCVideoElement
+}
