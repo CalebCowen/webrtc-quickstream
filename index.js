0122643eb918d4a8a6a86bbb99aeda59a90b78aa
Caleb <calebcowen@gmail.com>
2018-10-09 21:15:14 -0500

    add functions

---
+++ index.js
@@ -0,0 +1,42 @@
+if (navigator.mediaDevices === undefined) {
+  navigator.mediaDevices = {};
+}
+
+if (navigator.mediaDevices.getUserMedia === undefined) {
+  navigator.mediaDevices.getUserMedia = (constraints) => {
+
+    let getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia
+
+    if (!getUserMedia) {
+      return Promise.reject(new Error('getUserMedia is not implemented in this browser'))
+    }
+
+    return new Promise((resolve, reject) => {
+      getUserMedia.call(navigator, constraints, resolve, reject)
+    })
+  }
+}
+
+const getWebRTCVideoElement = async (height, width, video) => {
+  let isIOS = navigator.userAgent.includes('iPhone') || navigator.userAgent.includes('iPad')
+  let promise = isIOS ? getWebRTCStream() : getWebRTCStream({video: {height, width}})
+  promise.then((stream) => {
+    video.srcObject = stream
+    if (isIOS) {
+      video.height = height
+      video.width = width
+    }
+  })
+}
+
+const getWebRTCStream = async (e, constraints={video: true, audio: true}) => {
+  let stream = await navigator.mediaDevices.getUserMedia(constraints)
+  .then((stream) => stream)
+  .catch((err) => err)
+  return stream
+}
+
+exports = {
+  getWebRTCStream,
+  getWebRTCVideoElement
+}
