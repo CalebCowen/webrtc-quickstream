1dbe8f350293402430df28f9cb488edecd7155b2
Caleb <calebcowen@gmail.com>
2018-10-11 17:32:38 -0500

    return error

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const getWebRTCVideoElement = (video, height, width) => {
 
 const getWebRTCStream = async (constraints={video: true, audio: true}) => {
   let result = await navigator.mediaDevices.getUserMedia(constraints)
-  .then((stream) => stream).catch((err) => console.log(err)))
+  .then((stream) => stream).catch((err) => console.log(err))
   return result
 }
 
